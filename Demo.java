08b782ebb1f9ab889afacbc1aa0bdb41f0af8de6
sanrajbanokar <sanraj.banokar@gmail.com>
2020-12-21 14:56:10 +0530

    demo.java modified for loop ln removed

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
 	public static void main(String agrs[]){
 		System.out.println("Hello Sanraj bro!!");
 		for(int i =0; i<10;i++){
-			System.out.println(i);
+			System.out.print(i);
 		}
 	}
 }
